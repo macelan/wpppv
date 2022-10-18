5d5629e65fa77a6e838e2759b929adfff378a8a2
Marco Macelan <marcomacelan@yahoo.com.br>
2022-10-18 09:50:44 -0300

    3º Commite

---
+++ wp-content/themes/midiad/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portal Presidente Venveslau</title>
+</head>
+<body <?php body_class(); ?>>
+    <p>O Site do Venceslauence</p>
+    <header>
+		<section class="top-bar">
+			<div class="social-media-icons"></div>
+			<div class="search"></div>
+		</section>
+		<section class="menu-area">
+			<section class="logo"></section>
+			<nav class="menu"></nav>
+		</section>
+	</header>
--- wp-content/themes/midiad/index.php
+++ wp-content/themes/midiad/index.php
@@ -1,13 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Portal Presidente Venveslau</title>
-</head>
-<body>
-    <p>O Site do Venceslauence</p>
-
-</body>
-</html>
+    <?php get_header(); ?>
+    <div class="content-area">
+		<main>
+			<section class="slide"></section>
+			<section class="services"></section>
+			<section class="middle-area">
+				<aside class="sidebar"></aside>
+				<div class="news"></div>
+			</section>
+			<section class="map"></section>
+		</main>
+	</div>
+    <?php get_footer(); ?>
