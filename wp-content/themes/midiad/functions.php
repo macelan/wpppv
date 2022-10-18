3a9082ca2221b4b3cf26fb41a1320e80c18cbed8
Marco Macelan <marcomacelan@yahoo.com.br>
2022-10-18 16:14:26 -0300

    5º Commite

--- wp-content/themes/midiad/functions.php
+++ wp-content/themes/midiad/functions.php
@@ -1,8 +1,13 @@
 <?php 
 
-function load_scripts(){
-	wp_enqueue_style( 'template', get_template_directory_uri() . '/css/template.css', array(), '1.0', 'all' );
+function load_scripts(){ //carrega scripts
+	wp_enqueue_script( 'bootstrap-js', 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js', array(), '5.2.2', true );// Se usar a versão 5 passar o array vazio
+	wp_enqueue_style( 'bootstrap-css', 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css', array(), '5.2.2', 'all' );
+
+	//wp_enqueue_script( 'bootstrap-js', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js', array( 'jquery' ), '4.0.0', true );// Se usar a versão 5 passar o array vazio
+	//wp_enqueue_style( 'bootstrap-css', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css', array(), '4.0.0', 'all' );
+	wp_enqueue_style( 'template', get_template_directory_uri() . '/css/template.css', array(), '1.0', 'all' ); //wp_enqueue_style enfilera folhas de stylos
 }
-add_action( 'wp_enqueue_scripts', 'load_scripts' );
+add_action( 'wp_enqueue_scripts', 'load_scripts' );// 
 
 
