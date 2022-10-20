0fae159bf8004de9cd852b92da5d95ebfdcfa93a
Marco Macelan <marcomacelan@yahoo.com.br>
2022-10-20 15:50:47 -0300

    Ja tomando forma

--- wp-content/themes/midiad/header.php
+++ wp-content/themes/midiad/header.php
@@ -12,12 +12,24 @@
 			<div class="container">
 				<div class="row">
 					<div class="social-media-icons col-xl-9 col-sm-7 col-6">Ícones Sociais</div>
-					<div class="search col-xl-3 col-sm-5 col-6 text-end">Pesquisa</div>			
+					<div class="search col-xl-3 col-sm-5 col-6 text-right">Pesquisa</div>			
 				</div>
 			</div>
 		</section>
 		<section class="menu-area">
-			<section class="logo">Logo</section>
-			<nav class="menu">Menu</nav>
+			<div class="container">
+				<div class="row">
+					<section class="logo col-md-2 col-12 text-center">Logo</section>
+					<nav class="main-menu col-md-10 text-right">
+						<?php 
+						wp_nav_menu( 
+							array( 
+								'theme_location' => 'my_main_menu' 
+							) 
+						); 
+						?>
+					</nav>					
+				</div>
+			</div>
 		</section>
 	</header>
