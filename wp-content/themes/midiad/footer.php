2d024f7b33f84055ba32fdfdc32737e7037307b8
Marco Macelan <marcomacelan@yahoo.com.br>
2022-10-20 10:49:58 -0300

    Responsivo

--- wp-content/themes/midiad/footer.php
+++ wp-content/themes/midiad/footer.php
@@ -1,7 +1,7 @@
 <footer>
         <p>Este é mais um parágrafo de referência (Footer)</p>
 </footer>
-	
+	<?php wp_footer(); ?>
 
 </body>
 </html>
--- wp-content/themes/midiad/header.php
+++ wp-content/themes/midiad/header.php
@@ -8,9 +8,14 @@
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
-    <p>O Site do Venceslauence</p>
-    <header>
+       <header>
 		<section class="top-bar">
+		<div class="container">
+				<div class="row">
+					<div class="social-media-icons col-xl-9 col-sm-7 col-6">Ícones Sociais</div>
+					<div class="search col-xl-3 col-sm-5 col-6 text-end">Pesquisa</div>			
+				</div>
+		</div>
 			<div class="social-media-icons">Icones Sociais</div>
 			<div class="search">Pesquisa</div>
 		</section>
