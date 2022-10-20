6ee0ba0b9d7fceb5b4659630b72782888d19bdeb
Marco Macelan <marcomacelan@yahoo.com.br>
2022-10-20 16:44:37 -0300

    Posts

--- wp-content/themes/midiad/index.php
+++ wp-content/themes/midiad/index.php
@@ -26,8 +26,8 @@
 
 							<article>
 								<h2><?php the_title(); ?></h2>
-								<p>Published in <?php echo get_the_date(); ?> by <?php the_author_posts_link(); ?></p>
-								<p>Categories: <?php the_category( ' ' ); ?></p>
+								<p>Publicado em: <?php echo get_the_date(); ?> por <?php the_author_posts_link(); ?></p>
+								<p>Categorias: <?php the_category( ' ' ); ?></p>
 								<p><?php the_tags( 'Tags: ', ', ' ); ?></p>
 								<?php the_content(); ?>
 							</article>
